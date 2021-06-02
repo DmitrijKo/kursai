beeb6c30c5bb877ba16eb6c5cf29d13527f0c87f
DmitrijKo <kdimka666@gmail.com>
2021-06-02 21:14:32 +0300

    Namu darbai: antras vakaras

--- JavaScript/_namuDarbai/02/app.js
+++ JavaScript/_namuDarbai/02/app.js
@@ -36,26 +36,37 @@ console.log('Masyve reikšmių didesnių už 10 yra:', sum);
 
 
 // UŽDAVINYS Nr.2b
-// Raskite didžiausią masyvo reikšmę ir jos indeksą
+// Raskite didžiausią masyvo reikšmę ir jos indeksus
 console.log('UŽDAVINYS Nr.2b');
 
 console.log(arr);
-console.log('Didžiausią masyvo reikšmę yra: 25');
 
-arr.indexOf(25);
+let did = 5;
+let didIndex = [];
 
-console.log('Jos indeksas yra:', arr.indexOf(25));
+for (var i = 0; i < arr.length; i++) {
+   if (arr[i] > did) {
+      did = arr[i];
+      didIndex = []; // masyvo isvalymas
+   }
+   if (arr[i] === did) {
+      didIndex.push(i);
+   }
+}
+
+console.log('Didžiausią masyvo reikšmę yra:', did);
+console.log('Jų indeksai yra:', didIndex);
 
 
 // UŽDAVINYS Nr.2c
-// Suskaičiuokite visų porinių indeksų reikšmių sumą
+// Suskaičiuokite visų lyginių indeksų reikšmių sumą
 console.log('UŽDAVINYS Nr.2c');
 
 console.log(arr);
 var sum = 0;
 
 for (var i = 0; i < arr.length; i++) {
-   if (!(arr[i] % 2)) {
+   if (i % 2 === 0) {
       sum += arr[i];
    }
 }
@@ -131,7 +142,19 @@ console.log('Masyvas po pakeitimų:', arr);
 // Suraskite pirmą (mažiausią) indeksą, kurio elemento reikšmė didesnė už 10;
 console.log('UŽDAVINYS Nr.2h');
 
+console.log('Masyvas:', arr);
 
+let index1 = -1;
+let ii = 0;
+
+do {
+   if (arr[ii] > 10) {
+      index1 = ii;
+   }
+   ii++;
+} while (index1 === -1 && ii < arr.length);
+
+console.log('Pirmas indeksas, kurio elemento reikšmė didesnė už 10 yra:', index1);
 
 
 
@@ -139,10 +162,42 @@ console.log('UŽDAVINYS Nr.2h');
 // Naudodami funkciją splice() iš masyvo ištrinkite visus elementus turinčius porinį indeksą;
 console.log('UŽDAVINYS Nr.2i');
 
+console.log('Senas masyvas:', arr);
 
+for (var i = 0; i < arr.length; i++) {
+   arr.splice(i, 1);
+}
+
+console.log('Naujas masyvas:', arr);
 
 
 // UŽDAVINYS Nr.3
 // Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 200. 
 // Suskaičiuokite kiek yra kiekvienos raidės.
 console.log('UŽDAVINYS Nr.3');
+
+const abcd = ['A', 'B', 'C', 'D'];
+
+const arr3 = [];
+
+for (let i = 0; i < 200; i++) {
+    arr3.push(abcd[rand(0, 3)]);
+}
+
+console.log('Sugeneruotas masyvas iš raidžių yra:', arr3.slice());
+
+const counter = [0, 0, 0, 0];
+
+for (let i = 0; i < arr3.length; i++) {
+    counter[abcd.indexOf(arr3[i])]++;
+}
+
+let a = counter[0];
+let b = counter[1];
+let c = counter[2];
+let d = counter[3];
+
+console.log('A raidžių yra:', a);
+console.log('B raidžių yra:', b);
+console.log('C raidžių yra:', c);
+console.log('D raidžių yra:', d);
